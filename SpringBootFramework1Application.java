756860f08059513312f1fd30cabc3475674063fc
abhilashroyal29-afk <abhilashroyal29@gmail.com>
2026-05-22 14:30:45 +0530

    Add files via upload

---
+++ SpringBootFramework1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFramework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFramework1Application.class, args);
+	}
+
+}
